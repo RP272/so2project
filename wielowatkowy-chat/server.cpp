072839c5ee840526a41f32c4fc2a509eee4317f0
robibobi <robibobi@DESKTOP-322IV4E>
2025-04-11 18:18:14 +0200

    Created socket for server

--- wielowatkowy-chat/server.cpp
+++ wielowatkowy-chat/server.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <arpa/inet.h>
+
 int main(){
-    return 0;
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr; 
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    // Create an endpoint for communication. Return file descriptor for the new socket on success.
+    serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serv_sock == -1){
+        std::cerr << "socket() failed!" << std::endl;
+        exit(0);
+    }
 }
